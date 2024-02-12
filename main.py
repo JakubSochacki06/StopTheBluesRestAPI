7f040a7958186fc0ddd0256b3c9779eb5e0fed49
JakubSochacki06 <jakubsochacki06@gmail.com>
2024-02-12 19:31:03 +0100

    created RestAPI

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+from quotes import quotes
+import gunicorn
+@app.get("/")
+def welcome_text():
+    return {"Text":"Witamy w StopTheBluesAPI!"}
+
+@app.get("/randomQuote")
+def get_random_quote():
+    return quotes[random.randint(0,100)]
+
+if __name__ == "__main__":
+    app.run()
---
+++ quotes.py
@@ -0,0 +1,42 @@
+quotes = [
+    {
+        "quote": "Twoje obecne okoliczności nie określają, dokąd możesz dojść; określają jedynie, gdzie zaczynasz.",
+        "author": "Nido Qubein"
+    },
+    {
+        "quote": "Sukces to nie klucz do szczęścia. Szczęście to klucz do sukcesu. Jeśli kochasz to, co robisz, odniesiesz sukces.",
+        "author": "Albert Schweitzer"
+    },
+    {
+        "quote": "Życie to 10% tego, co nam się przytrafia, i 90% tego, jak na to reagujemy.",
+        "author": "Charles R. Swindoll"
+    },
+    {
+        "quote": "Prawdziwy sukces to radość życia.",
+        "author": "Daisaku Ikeda"
+    },
+    {
+        "quote": "W życiu chodzi nie o czekanie na burzę, ale o to, jak tańczysz podczas deszczu.",
+        "author": "Vivian Greene"
+    },
+    {
+        "quote": "Najlepszym sposobem przewidzenia przyszłości jest ją stworzyć.",
+        "author": "Peter Drucker"
+    },
+    {
+        "quote": "Sukces to osiągnięcie celów, które sobie postawiliśmy. Szczęście to pokochanie tego, co osiągnęliśmy.",
+        "author": "Jim Rohn"
+    },
+    {
+        "quote": "Nie ważne, jak wolno idziesz, ważne, żebyś się nie zatrzymał.",
+        "author": "Konfucjusz"
+    },
+    {
+        "quote": "Ciesz się chwilą, bo ta chwila stanie się wspomnieniem.",
+        "author": "Tenzin Gyatso"
+    },
+    {
+        "quote": "Kiedy myślisz, że osiągnąłeś swój limit, pamiętaj, że to tylko myśl.",
+        "author": "Bruce Lee"
+    }
+]
